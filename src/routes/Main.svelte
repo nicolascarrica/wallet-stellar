841203bd31bf15e5a577f2e71244b1a18d4c644b
Nicolas <nicolas.carrica@gmail.com>
2023-06-27 11:55:39 -0300

    create Main component

---
+++ src/routes/Main.svelte
@@ -0,0 +1,13 @@
+<script>
+  import AccountData from '../components/AccountData.svelte';
+
+  function handleLogout() {
+    signIn.set(false);
+    keyPair.set({ publicKey: '', secretKey: '' });
+  }
+
+</script>
+  <AccountData />
+
+
+  <button type="button" class="btn btn-outline-danger" on:click={handleLogout}>Logout</button>
